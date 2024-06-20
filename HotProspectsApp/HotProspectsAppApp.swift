5ec2043ed5fe8a3d3ba1fdb787c70a6203647152
Daria Yats <dariayatsyniuk@gmail.com>
2024-06-20 13:19:24 +0200

    Initial Commit

---
+++ HotProspectsApp/HotProspectsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsAppApp.swift
+//  HotProspectsApp
+//
+//  Created by Дарья Яцынюк on 20.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
